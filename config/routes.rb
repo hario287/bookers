d1e455eca587741ab0234f530dce0f4db7f8ffcd
EC2 Default User <ec2-user@ip-172-31-21-110.ap-northeast-1.compute.internal>
2022-09-14 12:33:03 +0000

    [Update] 課題2-Viewファイル訂正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get 'books/top'
-  get 'homes/top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
+  get 'books' => 'books#top'
+  resources :lists
+  #listsコントローラの同じ名前のアクションに紐づいてる
 end
