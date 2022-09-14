94017139b1c37d6b80edd8569acc0c05844057d1
EC2 Default User <ec2-user@ip-172-31-21-110.ap-northeast-1.compute.internal>
2022-09-14 18:44:55 +0000

    Confused

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'books/new'
   get 'top' => 'homes#top'
-  get 'books' => 'books#top'
-  resources :lists
-  #listsコントローラの同じ名前のアクションに紐づいてる
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  resources :books
+  #booksコントローラの同じ名前のアクションに紐づいてる
 end
