f696d239d165db210b61313b1e15ef97d81510b1
EC2 Default User <ec2-user@ip-172-31-21-110.ap-northeast-1.compute.internal>
2022-09-15 09:30:36 +0000

    課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,14 +6,13 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-   if
-    @book.save
-    flash[:notice] = "Book was successfully updated."
+    if @book.save
+    flash[:success] = "Book was successfully createdd."
     redirect_to "/books/#{@book.id}"
-  else
+    else
     @books = Book.all
     render "index"
-  end
+    end
 
   end
 
@@ -22,26 +21,34 @@ class BooksController < ApplicationController
      @book = Book.new
   end
 
+
+
+
   def show
     @book = Book.find(params[:id])
   end
 
   def edit
-    @book = Book.find(params[:id])
+   @book = Book.find(params[:id])
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
-    redirect_to "/books/#{book.id}"
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:success] = "Book was successfully updated."
+    redirect_to "/books/#{@book.id}"
+    else
+      render "edit"
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    flash[:success] = "Book was successfully destroyed."
+    redirect_to "/books"
   end
+
 
 private
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'books/new'
   get 'top' => 'homes#top'
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
   resources :books
-  #booksコントローラの同じ名前のアクションに紐づいてる
+  #booksコントローラreの同じ名前のアクションに紐づいてる
 end
